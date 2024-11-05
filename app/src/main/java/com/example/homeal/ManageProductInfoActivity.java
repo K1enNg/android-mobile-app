be002ec06de7b32393da1ee1e7877a47b3bad8cb
Kinkin <hiimkien2702@gmail.com>
2024-11-05 09:57:53 -0500

    create manage product info xml

--- app/src/main/java/com/example/homeal/ManageProductInfoActivity.java
+++ app/src/main/java/com/example/homeal/ManageProductInfoActivity.java
@@ -8,12 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SellerManageStoreActivity extends AppCompatActivity {
+public class ManageProductInfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_seller_manage_store);
+        setContentView(R.layout.activity_manage_product_info);
     }
 }

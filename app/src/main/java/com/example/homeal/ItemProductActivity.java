08e2958792396fed486e1e6f11b73450cdd7e60f
Kinkin <hiimkien2702@gmail.com>
2024-11-11 08:55:40 -0500

    build buyer ui

--- app/src/main/java/com/example/homeal/ItemProductActivity.java
+++ app/src/main/java/com/example/homeal/ItemProductActivity.java
@@ -1,17 +1,19 @@
 package com.example.homeal;
 
 import android.os.Bundle;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
-
-public class LoginActivity extends AppCompatActivity {
-
+public class ItemProductActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_item_product);
     }
 }
